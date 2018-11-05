af0d140f79d84043b1cc1d8ea19ecb522f336654
WhiteChimney <44578389+WhiteChimney@users.noreply.github.com>
2018-11-05 16:50:44 +0800

    Update DynamicFFT_multiChn.m

--- DynamicFFT_multiChn.m
+++ DynamicFFT_multiChn.m
@@ -7,43 +7,37 @@ clear sound
 
 
 %% Read in audio file and preset parameters
-directory = 'C:\Users\79082\MATLAB Drive\Signals and Systems\sound_project\';
+directory = 'C:\Users\sound_project\';
 filename = 'Ennio Morricone - The Crave.mp3';
-% filename = 'Steve¡¤Feng - Flying Bug (Piano Version).flac';
-% filename = 'Megalovania (Piano Version).flac';
-% filename = 'Jeno Jando - 12 Variations in C Major on Ah vous dirai-je, maman, K. 265.flac';
-% filename = 'Arthur Rubinstein - Op. 27 - Sonata No. 14 - Moonlight - 3 Presto agitato.mp3';
-% filename = 'Lang Project - The Crave (Piano Version) [From The Legend of 1900].flac';
-% filename = 'Fr¨¦d¨¦ric Chopin 12 Etudes, Op.25 - No. 11 in A minor Winter Wind.mp3';
-% filename = 'Adam Levine - Lost Stars.flac';
-% filename = 'C3-1-C4.wav';
-% filename = 'C1-7-C6.wav';
-% filename = '(C1+C2+C3)(C4+C5+C6).wav';
-% filename = '(C3+C4)-1-(C4+C5).wav';
-% filename = 'Drum.wav';
 
 
+DispMode = 1;                           % 0: display choosed channel (chn0)
+                                        % 1: display all channels in a same plot
+                                        % 2: display differenct channels seperately
 chn0 = 1;                               % choose the channel of the music
-DispMode = 1;                           % 1 for seperate, 2 for comparison (only for dual channels)
-Fs1 = 44.1e3;                           % sampling freq, 44.1kHz by default
+Fs1 = 44.1e3;                           % resampling freq
 Spd = 1.00;                             % playing speed
 Tr = 0.03;          % Approx refresh time interval (when music is playing 
                     % in normal speed), will be adjusted due to 
                     % consideration of the efficiency of FFT algorithm.
                     % If your pic keeps falling behind your sound, 
                     % please increase this parameter.
-FreqRange = [0 1e4];                   % Frequency range
+FreqRange = [0 1e4];                    % Frequency range
 AmpRange = [0 200];                     % Amplitude range
 T_delay = 0.00;                         % delay the pic to sync, >=0
 
+
 %% FFT of each segment of the sample music
-[Msc0, Fs0] = audioread([directory, filename]);  % read audio file
-if DispMode == 0
-    Msc1 = resample(Msc0(:,chn0), Fs1, Fs0);        % resampling
+[Msc0, Fs0] = audioread([directory, filename]);     % read audio file
+if DispMode == 0                                    % resampling
+    Msc1 = resample(Msc0(:,chn0), Fs1, Fs0);
+elseif DispMode == 0 || DispMode == 1
+    Msc1 = resample(Msc0, Fs1, Fs0);
 else
-    Msc1 = resample(Msc0, Fs1, Fs0);        % resampling
+    disp('Choose the right Display Mode!');
+    return;
 end
-[N, chns] = size(Msc1);                  % number of sampling points
+[N, chns] = size(Msc1);                 % number of sampling points
 dt = 1/Fs1;                             % time interval
 Lpc = 2^nextpow2(Tr*Fs1);               % length of each piece of music
 Nf = Lpc/2 + 1;                         % number of points in freq domain
@@ -60,7 +54,7 @@ if DispMode == 0 || DispMode == 1
     legend(legend_list);
     xlabel('Frequency/Hz');
     ylabel('Relative Amplitude');
-elseif DispMode == 2
+else
     for i = 1:chns
         subplot(chns,1,i);
         eval(['p',num2str(i),'=semilogx(f,Freq(:,i));']);
@@ -72,9 +66,6 @@ elseif DispMode == 2
         ylabel('Relative Amplitude');
     end
     suptitle(filename);
-else
-    disp('Choose the right Display Mode!');
-    return;
 end    
 
 % Start the loop
@@ -88,7 +79,7 @@ while Pt < N                            % while the song is not over
     % FFT of music pieces
     MscPc = Msc1(Pt-Lpc+1:Pt,:);        % the present piece of music
     Freq = fft(MscPc);                  % FFT of the piece of music
-    Freq = abs(Freq(1:Lpc/2+1,:));   % real amp is abs(fft(x)) * dt
+    Freq = abs(Freq(1:Lpc/2+1,:));      % real amp is abs(fft(x)) * dt
 
     % Sync of pic and sound
     if Pt == Lpc                        % the first piece of music
@@ -104,7 +95,7 @@ while Pt < N                            % while the song is not over
     end
     T0 = T0 + Lpc/Fs1/Spd;
     
-    % draw the pic
+    % update data points
     if DispMode == 0 || DispMode == 1
         set(p, {'YData'}, num2cell(Freq.',2) )
     else
